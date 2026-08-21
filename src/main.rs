2f3436c774187dde8f5c14a915bd4fe45744b239
koki <kokieg321@gmail.com>
2026-08-22 01:59:24 +0900

    Report escape marker and last widget in ZLE state dump

--- src/main.rs
+++ src/main.rs
@@ -2133,6 +2133,7 @@ if [[ -o interactive ]] && (( $+commands[zsuggestion] )); then
   }
 
   _zsuggestion_escape() {
+    typeset -g _ZSUGGESTION_ESCAPE_RAN=1
     if (( _ZSUGGESTION_FUZZY_ACTIVE )); then
       local base="$_ZSUGGESTION_FUZZY_BASE"
       _zsuggestion_menu_clear
